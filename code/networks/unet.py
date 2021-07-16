b2edda07da7d6b2aa75b36a592ccfb7ebc827ba1
luoxiangde <SENSETIME\luoxiangde@cn0614009451l.domain.sensetime.com>
2021-07-16 21:37:07 +0800

    reimplemented the Cross_Pseudo_Supervision for medical image application

--- code/networks/unet.py
+++ code/networks/unet.py
@@ -9,7 +9,25 @@ import torch
 import torch.nn as nn
 from torch.distributions.uniform import Uniform
 
+def kaiming_normal_init_weight(model):
+    for m in model.modules():
+        if isinstance(m, nn.Conv3d):
+            torch.nn.init.kaiming_normal_(m.weight)
+        elif isinstance(m, nn.BatchNorm3d):
+            m.weight.data.fill_(1)
+            m.bias.data.zero_()
+    return model
 
+def sparse_init_weight(model):
+    for m in model.modules():
+        if isinstance(m, nn.Conv3d):
+            torch.nn.init.sparse_(m.weight, sparsity=0.1)
+        elif isinstance(m, nn.BatchNorm3d):
+            m.weight.data.fill_(1)
+            m.bias.data.zero_()
+    return model
+
+    
 class ConvBlock(nn.Module):
     """two convolution layers with batch norm and leaky relu"""
 
