1ad149d7c90ccedbee35f00ca3747243f9847f6a
luoxiangde <SENSETIME\luoxiangde@cn0614009451l.domain.sensetime.com>
2021-07-22 20:45:38 +0800

    upload the brats2019 training and testing shell

--- code/test_brats2019_semi_seg.sh
+++ code/test_brats2019_semi_seg.sh
@@ -6,4 +6,5 @@ python -u test_3D.py --root_path ../data/BraTS2019 --exp BraTS2019/Uncertainty_A
 python -u test_3D.py --root_path ../data/BraTS2019 --exp BraTS2019/Interpolation_Consistency_Training_25 --model unet_3D && 
 python -u test_3D.py --root_path ../data/BraTS2019 --exp BraTS2019/Entropy_Minimization_25 --model unet_3D && 
 python -u test_3D.py --root_path ../data/BraTS2019 --exp BraTS2019/Cross_Pseudo_Supervision_25 --model unet_3D && 
+python -u test_3D.py --root_path ../data/BraTS2019 --exp BraTS2019/Adversarial_Network_25 --model unet_3D && 
 python -u test_3D.py --root_path ../data/BraTS2019 --exp BraTS2019/Uncertainty_Rectified_Pyramid_Consistency_25 --model unet_3D_dv_semi
