63d6c8dd86bc22212756c1a35fb98683cf6633db
Govind Jeevan <govindjeevan7@gmail.com>
2022-01-04 12:48:20 +0530

    val 2d support for multiple outputs

--- code/val_2D.py
+++ code/val_2D.py
@@ -23,12 +23,18 @@ def test_single_volume(image, label, net, classes, patch_size=[256, 256]):
         slice = image[ind, :, :]
         x, y = slice.shape[0], slice.shape[1]
         slice = zoom(slice, (patch_size[0] / x, patch_size[1] / y), order=0)
-        input = torch.from_numpy(slice).unsqueeze(
-            0).unsqueeze(0).float().cuda()
+        input = torch.from_numpy(slice).unsqueeze(0).unsqueeze(0).float().cuda()
         net.eval()
         with torch.no_grad():
-            out = torch.argmax(torch.softmax(
-                net(input), dim=1), dim=1).squeeze(0)
+            out = net(input)
+            if len(out)==2:
+                out = torch.softmax(out[1], dim=1)
+            elif len(out)==1:
+                out = torch.softmax(out, dim=1)
+            else:
+                out = torch.softmax(out[0], dim=1)
+                
+            out = torch.argmax(out, dim=1).squeeze(0)
             out = out.cpu().detach().numpy()
             pred = zoom(out, (x / patch_size[0], y / patch_size[1]), order=0)
             prediction[ind] = pred
@@ -47,13 +53,11 @@ def test_single_volume_ds(image, label, net, classes, patch_size=[256, 256]):
         slice = image[ind, :, :]
         x, y = slice.shape[0], slice.shape[1]
         slice = zoom(slice, (patch_size[0] / x, patch_size[1] / y), order=0)
-        input = torch.from_numpy(slice).unsqueeze(
-            0).unsqueeze(0).float().cuda()
+        input = torch.from_numpy(slice).unsqueeze(0).unsqueeze(0).float().cuda()
         net.eval()
         with torch.no_grad():
             output_main, _, _, _ = net(input)
-            out = torch.argmax(torch.softmax(
-                output_main, dim=1), dim=1).squeeze(0)
+            out = torch.argmax(torch.softmax(output_main, dim=1), dim=1).squeeze(0)
             out = out.cpu().detach().numpy()
             pred = zoom(out, (x / patch_size[0], y / patch_size[1]), order=0)
             prediction[ind] = pred
